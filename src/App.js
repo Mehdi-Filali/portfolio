417f8befa987ce0fdee542647632b349efe8f8ae
Filali Mehdi <aniq.filali.mehdi@gmail.com>
2022-08-04 14:51:18 +0200

    responsive correction

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/web-portfolio/" element={<Home />} />
+          <Route path="/web-portfolio/skills" element={<Skills />} />
+          <Route path="/web-portfolio/contact" element={<Contact />} />
+          <Route path="/web-portfolio/portfolio" element={<Portfolio />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
